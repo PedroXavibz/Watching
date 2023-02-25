9fcb11d4bd637ab91ab26f6db7007b5a74c67531
Pedro <joao.pedro.xavibz@gmail.com>
2023-02-25 18:42:52 -0300

    Added snackbar and session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { SessionProvider } from 'next-auth/react';
+import SnackbarProvider from '@/contexts/useSnackbar.context';
 import { Provider } from 'react-redux';
 import store from '@/store';
 
 import '@/styles/globals.css';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
-  <SessionProvider session={session} >
+  <SessionProvider session={session}>
     <Provider store={store}>
-      <Component {...pageProps} />
+      <SnackbarProvider>
+        <Script
+          src="https://www.youtube.com/player_api"
+          strategy="lazyOnload"
+        />
+        <Component {...pageProps} />
+      </SnackbarProvider>
     </Provider>
-  </SessionProvider >
+  </SessionProvider>
 );
 
 export default App;
