65fea646e8e251d5f06734980e408182d97d028b
Pedro <joao.pedro.xavibz@gmail.com>
2023-02-25 19:07:38 -0300

    Added room and video slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '@/slices/userSlice';
+import roomSlice from '@/slices/roomSlice';
+import videoSlice from '@/slices/videoSlice';
 
 const store = configureStore({
   reducer: {
-    user: userSlice
-  }
+    user: userSlice,
+    room: roomSlice,
+    video: videoSlice,
+  },
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
